a42a23be6efa7d981ce965124e4aa20640e1f33c
akku <akk@mirai.re>
2023-11-25 12:51:46 +0900

    いろいろ

--- app/info/config.py
+++ app/info/config.py
@@ -1,8 +1,8 @@
 import tomllib
 from pathlib import Path
-from lib.dict2 import dict2
+from lib.dict2 import Dict2
 
 path = Path(__file__).parent.parent.parent/"config"/"default.toml"
 
 with open(path, 'rb') as fin:
-  conf = dict2(tomllib.load(fin))
+  conf = Dict2(tomllib.load(fin))
--- app/lib/dict2.py
+++ app/lib/dict2.py
@@ -1,5 +1,5 @@
 # dict["key"] だけでなく dict.key でもアクセスできる辞書
-class dict2(dict): 
+class Dict2(dict): 
     def __init__(self, *args, **kwargs): 
         super().__init__(*args, **kwargs) 
         self.__dict__ = self 
