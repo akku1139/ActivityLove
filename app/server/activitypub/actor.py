3b10ae559df7408ae092532d4bdc9d3c5c48ee6e
akku <akk@mirai.re>
2023-12-02 11:30:13 +0900

    インデント修正

--- app/server/activitypub/actor.py
+++ app/server/activitypub/actor.py
@@ -3,26 +3,27 @@ from starlette.requests import Request
 from lib.activityjson import ActivityJSONResponse
 from starlette.responses import RedirectResponse
 from info.config import conf
+from db import db
 
 async def actor(request:Request) -> ActivityJSONResponse:
   return ActivityJSONResponse({
-      "followers": f'https://{conf["host"]}/user/{request.path_params["id"]}/followers',
-      "following": f'https://{conf["host"]}/user/{request.path_params["id"]}/following',
-      "icon": {
-      },
-      "id": f'https://{conf["host"]}/user/{request.path_params["id"]}',
-      "inbox": f'https://{conf["host"]}/user/{request.path_params["id"]}/inbox',
-      "name": "Actorの名前",
-      "outbox": f'https://{conf["host"]}/user/{request.path_params["id"]}/outbox',
-      "preferredUsername": "",
-      "publicKey": {
-        # わからないので後で
-      },
-      "summary": "Actorの説明",
-      "type": "Person",
-      "url": f'https://{conf["host"]}/user/{request.path_params["id"]}',
-      "discoverable": False
-    }, media_type="application/xrd+xml")
+    "followers": f'https://{conf["host"]}/user/{request.path_params["id"]}/followers',
+    "following": f'https://{conf["host"]}/user/{request.path_params["id"]}/following',
+    "icon": {
+    },
+    "id": f'https://{conf["host"]}/user/{request.path_params["id"]}',
+    "inbox": f'https://{conf["host"]}/user/{request.path_params["id"]}/inbox',
+    "name": "Actorの名前",
+    "outbox": f'https://{conf["host"]}/user/{request.path_params["id"]}/outbox',
+    "preferredUsername": "",
+    "publicKey": {
+      # わからないので後で
+    },
+    "summary": "Actorの説明",
+    "type": "Person",
+    "url": f'https://{conf["host"]}/user/{request.path_params["id"]}',
+    "discoverable": False
+  }, media_type="application/xrd+xml")
 
 routes = [
   # Route("/user/{id: str}/actor", actor)
