d2db7b5022595ffa1a44ff40a6fedc697417ea72
-akku- <151517253+akku1139@users.noreply.github.com>
2023-12-13 09:10:03 +0000

    正しくルーティングを修正

--- src/autoroute.py
+++ src/autoroute.py
@@ -2,21 +2,25 @@ import glob
 from types import ModuleType
 from starlette.routing import Route
 
+special_route:dict = {
+  "well-known": ".well-known",
+  "__init__": "",
+  "2_0": "2.0",
+  "2_1": "2.1",
+  "_json": ".json",
+}
+
 routes:list = []
 # forはスコープを作らない
 methods:list = []
 
 for p in glob.glob("**/*.py", root_dir="route", recursive=True):
-  print(p)
   mod:ModuleType = __import__("route."+p.removesuffix(".py").replace('/', '.'))
   try:
     methods = mod.methods
   except:
     methods = ["GET"]
-  path:str = p \
-    .replace("well-known", ".well-known") \
-    .replace("__init__", "/") \
-    .replace("2_0", "2.0") \
-    .replace("2_1", "2.1") \
-    .replace("_json", ".json")
-  routes.append(Route("/"+p, endpoint=mod.endpoint, methods=methods))
+  path:str = "/" + (p.removesuffix(".py"))
+  for r in special_route:
+    path = path.replace(r, special_route[r])
+  routes.append(Route(path, endpoint=mod.endpoint, methods=methods))
