07809b8496bd0ff39ff953171420b17fb7d0581e
-akku- <151517253+akku1139@users.noreply.github.com>
2023-12-13 09:34:17 +0000

    全部同じEndpointがロードされるのを阻止

--- src/autoroute.py
+++ src/autoroute.py
@@ -1,6 +1,7 @@
 import glob
 from types import ModuleType
 from starlette.routing import Route
+import importlib
 
 special_route:dict = {
   "well-known": ".well-known",
@@ -13,14 +14,20 @@ special_route:dict = {
 routes:list = []
 # forはスコープを作らない
 methods:list = []
+mod:ModuleType
 
 for p in glob.glob("**/*.py", root_dir="route", recursive=True):
-  mod:ModuleType = __import__("route."+p.removesuffix(".py").replace('/', '.'))
+  #mod = __import__("route."+p.removesuffix(".py").replace('/', '.'))
+  mod = importlib.import_module("route."+p.removesuffix(".py").replace('/', '.'))
   try:
     methods = mod.methods
   except:
     methods = ["GET"]
+
   path:str = "/" + (p.removesuffix(".py"))
   for r in special_route:
     path = path.replace(r, special_route[r])
+
+  print(f'mod: {"route."+p.removesuffix(".py").replace('/', '.')}\npath: {path}')
+  print(mod)
   routes.append(Route(path, endpoint=mod.endpoint, methods=methods))
