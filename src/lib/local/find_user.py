f07c06d095ad83548ab86bb3109cc34155262879
-akku- <151517253+akku1139@users.noreply.github.com>
2023-12-27 10:41:41 +0900

    typo

--- src/lib/local/find_user.py
+++ src/lib/local/find_user.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from db.model.user import User
 
 async def find_user(db:AsyncSession, id:str) -> bool:
-  if (await (db.excute(select(User).filter(User.id == id)))) is None:
+  if (await (db.execute(select(User).filter(User.id == id)))) is None:
     return False
   else:
     return True
