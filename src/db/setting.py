15929d4e7d29a3e55393a41879b9cc859efc149c
-akku- <151517253+akku1139@users.noreply.github.com>
2023-12-27 10:12:14 +0900

    多分

--- src/db/setting.py
+++ src/db/setting.py
@@ -37,9 +37,9 @@ engine = create_async_engine(
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 class Base(DeclarativeBase):
-    pass
+  pass
 
 
 async def get_session():
-    async with async_session() as session:
-        yield session
+  async with async_session() as session:
+    yield session
--- src/route/user/{id:str}/actor.py
+++ src/route/user/{id:str}/actor.py
@@ -2,29 +2,29 @@ from starlette.requests import Request
 from lib.activityjson import ActivityJSONResponse
 from starlette.responses import Response, RedirectResponse
 from info.config import conf
-from db.setting import get_session
+from db.setting import async_session
 from lib.local.find_user import find_user
 
 async def endpoint(request:Request) -> ActivityJSONResponse | Response:
-  db = await get_session()
-  if(await find_user(db, request.path_params["id"])):
-    return ActivityJSONResponse({
-      "followers": f'https://{conf["host"]}/user/{request.path_params["id"]}/followers',
-      "following": f'https://{conf["host"]}/user/{request.path_params["id"]}/following',
-      "icon": {
-      },
-      "id": f'https://{conf["host"]}/user/{request.path_params["id"]}/actor',
-      "inbox": f'https://{conf["host"]}/user/{request.path_params["id"]}/inbox',
-      "name": "Actorの名前",
-      "outbox": f'https://{conf["host"]}/user/{request.path_params["id"]}/outbox',
-      "preferredUsername": "",
-      "publicKey": {
-        # わからないので後で
-      },
-      "summary": "Actorの説明",
-      "type": "Person",
-      "url": f'https://{conf["host"]}/user/{request.path_params["id"]}',
-      "discoverable": False # これもよくわからん
-    }, media_type="application/activity+json")
-  else:
-    return Response("User not found", status_code=404)
+  async with async_session() as db:
+    if(await find_user(db, request.path_params["id"])):
+      return ActivityJSONResponse({
+        "followers": f'https://{conf["host"]}/user/{request.path_params["id"]}/followers',
+        "following": f'https://{conf["host"]}/user/{request.path_params["id"]}/following',
+        "icon": {
+        },
+        "id": f'https://{conf["host"]}/user/{request.path_params["id"]}/actor',
+        "inbox": f'https://{conf["host"]}/user/{request.path_params["id"]}/inbox',
+        "name": "Actorの名前",
+        "outbox": f'https://{conf["host"]}/user/{request.path_params["id"]}/outbox',
+        "preferredUsername": "",
+        "publicKey": {
+          # わからないので後で
+        },
+        "summary": "Actorの説明",
+        "type": "Person",
+        "url": f'https://{conf["host"]}/user/{request.path_params["id"]}',
+        "discoverable": False # これもよくわからん
+      }, media_type="application/activity+json")
+    else:
+      return Response("User not found", status_code=404)
