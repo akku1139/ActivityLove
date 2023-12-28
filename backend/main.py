437ed91a96236637a661f834dedeec294735b7d5
-akku- <151517253+akku1139@users.noreply.github.com>
2023-12-29 01:06:42 +0900

    多分SvelteKitがSPAモードで動くように。 SPAにしたら /user/name とかで参照できない!っていう指摘は今は無し。

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,22 @@
 from starlette.applications import Starlette
+from starlette.exceptions import HTTPException
+from starlette.requests import Request
+from starlette.responses import FileResponse
+from starlette.staticfiles import StaticFiles
 import autoroute
+
+def index(request: Request, exc: HTTPException):
+  return FileResponse("../frontend/build/200.html", media_type="text/html")
+
+exception_handlers = {
+  404: index
+}
 
 app = Starlette(
   debug=False,
-  routes=autoroute.routes,
+  routes=[
+    Mount('/', app=StaticFiles(directory="../frontend/build", html=True), name="frontend"),
+    autoroute.routes
+  ],
+  exception_handlers=exception_handlers
 )
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,5 @@ import { defineConfig } from 'vite';
 import path from 'path'
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		}
-	}
+	plugins: [sveltekit()]
 });
