dcdf5362a6f8dc1179d87b1364a0daa8b80a922d
-akku- <151517253+akku1139@users.noreply.github.com>
2023-12-28 22:12:12 +0900

    Svelteのテンプレートを移植

---
+++ frontend/vite.config.ts
@@ -0,0 +1,13 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+import path from 'path'
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		}
+	}
+});
